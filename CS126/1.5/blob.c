e1f0c5e1de198d016a2c62ece0c4a77e77ef3cdb
Yat-o <tylergoodmen312@gmail.com>
2023-12-11 22:21:58 -0500

    CS126 | 1.5 Recode

--- CS126/1.5/blob.c
+++ CS126/1.5/blob.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    int num1; // declartions
-    int num2;
-    int multiply;
-    int sub;
-    int add; 
+void main() {
+    int num1, num2, multiply, add, sub;
 
-    printf("Enter a number: ");
+    printf("Enter the first number: ");
     scanf("%d", &num1);
-
-    printf("Enter another number: ");
+    printf("Enter the second number: ");
     scanf("%d", &num2);
 
     multiply = num1 * num2;
     sub = num1 - num2;
-    add = num1 - num2;
+    add = num1 + num2;
 
     printf("The product of both numbers is: %d\n", multiply);
     printf("The sum of both numbers is: %d\n", add);
     printf("The difference of both numbers is: %d\n", sub);
-    return 0;
 }
